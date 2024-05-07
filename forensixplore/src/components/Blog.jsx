e139ed7e9ec97ba12c4001561712cb8e9170fdea
niranjan87 <2100090187csit@gmail.com>
2024-05-07 08:04:49 +0530

    css

--- forensixplore/src/components/Blog.jsx
+++ forensixplore/src/components/Blog.jsx
@@ -9,7 +9,7 @@ function Blog() {
     <Navbar></Navbar>
       {/* START SECTION TOP */}
       <section className="section-top">
-        <div className="container">
+        <div className="container2">
           <div className="col-lg-10 offset-lg-1 text-center">
             <div className="section-top-title wow fadeInRight" data-wow-duration="1s" data-wow-delay="0.3s" data-wow-offset="0" >
               <h1>Latest Blog</h1>
@@ -24,10 +24,7 @@ function Blog() {
       {/* END SECTION TOP */}
 
       <div className="card-header">
-        <div className="card">
-
-
-
+        <div className="card2">
 
           <div className="nft">
             <div className='main'>
